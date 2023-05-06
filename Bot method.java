579428a6fac7e2be7b84d85653c681dd396717ea
MJNderMJWN <93721943+NderiJohn@users.noreply.github.com>
2023-05-06 10:34:49 +0300

    Create Bot method.java

---
+++ Bot method.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Program {
+    //your code goes here
+    /*
+    User message: "1", Reply: "Order confirmed"
+    User message: "2", Reply: "info@sololearn.com"
+    For any other number, the reply should be: "Try again". 
+    */
+    static void bot (){
+        Scanner sc = new Scanner (System.in);
+        int input = sc.nextInt ();
+        if (input == 1) {
+            System.out.println ("Order confirmed");
+        } else if (input == 2) {
+            System.out.println ("info@sololearn.com");
+        } else {
+            System.out.println ("Try again");
+        }
+    }
+    public static void main(String[] args) {
+        bot();
+    }
+}
